3e98dc8f904b4bb5e923e839330cd89a59e4efbb
Simon Spange <simon.spange@hotmail.com>
2024-06-07 22:32:01 +0200

    Oprydning

--- src/app.py
+++ src/app.py
@@ -7,22 +7,23 @@ import config
 app = Flask(__name__)
 
 # Database configuration
-DB_NAME = config.DB_NAME#'your_db_name'
-DB_USER = config.DB_USER#'your_db_user'
-DB_PASS = config.DB_PASS#'your_db_password'
-DB_HOST = config.DB_HOST#'your_db_host'
-DB_PORT = config.DB_PORT#'your_db_port'
+DB_NAME = config.DB_NAME    #'your_db_name'
+DB_USER = config.DB_USER    #'your_db_user'
+DB_PASS = config.DB_PASS    #'your_db_password'
+DB_HOST = config.DB_HOST    #'your_db_host'
+DB_PORT = config.DB_PORT    #'your_db_port'
 
-# Function to get a database connection
-def get_db_connection():
-    conn = psycopg2.connect(
+# set your own database name, username and password
+db = "dbname='Crypto' user='postgres' host='localhost' password='Krelle2024'" #potentially wrong password
+conn = psycopg2.connect(
         dbname=DB_NAME,
         user=DB_USER,
         password=DB_PASS,
         host=DB_HOST,
         port=DB_PORT
     )
-    return conn
+cursor = conn.cursor()
+
 
 @app.route('/')
 def index():
