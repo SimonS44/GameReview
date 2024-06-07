36ccc17606e1a9f25f14dc155b833f8087a86466
rgj736 <rgj736@alumni.ku.dk>
2024-06-07 22:30:52 +0200

    Delete config.py

--- src/config.py
+++
@@ -1,5 +0,0 @@
-DB_NAME = 'postgres'#'your_db_name'
-DB_USER = 'postgres'#'your_db_user'
-DB_PASS = 'Krelle2024'#'your_db_password'
-DB_HOST = 'localhost'#'your_db_host'
-DB_PORT = '5432'#'your_db_port'
