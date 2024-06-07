8bdefe887e67ca2577c175a2372de065265c9842
Simon Spange <simon.spange@hotmail.com>
2024-06-07 22:26:50 +0200

    git ignore config.py in future pelase

--- src/app.py
+++ src/app.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, request, jsonify
 import psycopg2
 import pandas as pd
 import os
+import config
 
 app = Flask(__name__)
 
 # Database configuration
-DB_NAME = 'postgres'#'your_db_name'
-DB_USER = 'postgres'#'your_db_user'
-DB_PASS = 'Krelle2024'#'your_db_password'
-DB_HOST = 'localhost'#'your_db_host'
-DB_PORT = '5432'#'your_db_port'
+DB_NAME = config.DB_NAME#'your_db_name'
+DB_USER = config.DB_USER#'your_db_user'
+DB_PASS = config.DB_PASS#'your_db_password'
+DB_HOST = config.DB_HOST#'your_db_host'
+DB_PORT = config.DB_PORT#'your_db_port'
 
 # Function to get a database connection
 def get_db_connection():
